68914bf8dfb040e4231735efc11715a5fd28014c
Gajendra Singh Pawar <gajendra.pawar@rsvrtech.com>
2021-06-21 12:14:58 +0530

    changes

---
+++ app/Models/Apps.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Apps extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    use SoftDeletes;
+
+    protected $hidden = ['deleted_at'];
+
+    protected $dates = [ 'created_at','updated_at','deleted_at'];
+
+    protected $fillable = [
+        'app_id',
+        'app_name',
+        'app_desc',
+        'app_link'
+    ];
+}
